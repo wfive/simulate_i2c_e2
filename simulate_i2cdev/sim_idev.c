640b044ac984687cfbfef4d56dae8cadc9101910
gohi <wfivenx@163.com>
2021-03-29 21:53:12 +0800

    Summary:simulate_i2cdev done
    who: gohi
    why: simulate_i2cdev donee
    what: NA

--- simulate_i2cdev/sim_idev.c
+++ simulate_i2cdev/sim_idev.c
@@ -15,6 +15,8 @@
 
 static int debug = 0;
 module_param(debug, int, S_IRUGO);
+/* 设置为模块参数，，模块加载后我们可以在/sys/module/模块名/parameters/目录下面
+ * 看到一个名为debug的文件，通过echo 1 > debug来改变这个变量的值 */
 MODULE_PARM_DESC(debug, "\n"
         "\t\tWhen set debug. This allows debug information output\n");
 
@@ -28,12 +30,22 @@ static sim_idev_data_t sim_idev_pdata = {
     .wr_protect_addr = 0x56,
 };
 
+/* 内核框架限制，必须要有release函数，用于卸载设备的时候释放资源，此处我们并无资源需要主动释放，
+ * 加个debug信息，调试的时候可以查看。*/
+static void sim_idev_pdev_release(struct device *dev)
+{
+    IDEV_DBG("Simulate i2c platform device release.\n");
+    return;
+}
+
 static struct platform_device g_sim_ipdev = {
     .name           = "sim_idev",
+    /* 赋值为-1，设备的id号将会由系统分配，如果有需要我们可以指定ID */
     .id             = -1,
     .dev = {
         /* 存放设备私有的资源信息，在driver中可以获取到 */
         .platform_data = &sim_idev_pdata,
+        .release = sim_idev_pdev_release,
     }
 };
 
@@ -45,9 +57,15 @@ static int __init sim_idev_init(void)
      * 我们写app的时候开头就是include <stdio.h>,printf就定义在那里面，
      * 内核不一样，没有stdio.h这些标准C库的，但是内核有自己实现的一些常用接口，比如打印就是printk */
     printk(KERN_ALERT "Start sim_idev_init\n");
+    /* 注册平台设备做为模拟I2C控制器的软件抽象成设备,&g_sim_ipdev是区g_sim_ipdev全局变量的地址，
+     * 这样 &g_sim_ipdev就是一个指针，注册接口的定义是 int platform_device_register(struct platform_device *) */
     ret = platform_device_register(&g_sim_ipdev);
     if (ret) {
         IDEV_DBG("Register platform device failed:%d\n", ret);
+        /* goto在一般c语言教程中都会不建议使用，但是在内核里面出现频率是很高的，
+         * 基本上就是一个用途，做函数返回，在错误的时候直接跳转到相应的出口，
+         * 函数中出口比较多的情况下用goto会比较好处理并且直观
+         */
         goto err;
     }
 
@@ -58,6 +76,8 @@ err:
 /* 整体和init的类似，函数返回的是void，没有返回值，函数return不带任何东西表示返回void */
 static void __exit sim_idev_exit(void)
 {
+    /* 卸载设备 */
+    platform_device_unregister(&g_sim_ipdev);
     return;
 }
 /* 内核定义好的接口，直接调用，我们模块加载和卸载时候就会调用传入的接口
--- simulate_i2cdev/sim_idev.h
+++ simulate_i2cdev/sim_idev.h
@@ -4,10 +4,15 @@
 	> Mail: 
 	> Created Time:
  ************************************************************************/
-
+/* 如果没有定义_SIM_IDEV_H就定义_SIM_IDEV_H，然后引用该文的定义
+ * 这样做可以避免一个C文件中头文件嵌套include多次导致数据、宏等重复定义 */
 #ifndef _SIM_IDEV_H
 #define _SIM_IDEV_H
 
+/* 用宏封装我们的debug打印接口，我们定义一个模块debug的变量，通过更改debug变量的值来控制debug信息输出。
+ * args..表示第一个参数传给fmt,参入参数可能为多个。 ##args表示args...中的多个参数，串连起来
+ * 宏定义中用do...while(0)将if包裹起来是为了避免在条件语句里面引用的时候导致乱入的if嵌套
+ */
 #define IDEV_DBG(fmt, arg...)  \
     do {                       \
       if (debug) {             \
@@ -15,6 +20,7 @@
       }                        \
     } while (0)
 
+/* typedef 的作用就是定义 sim_idev_data_t 为 struct sim_idev_data */
 typedef struct sim_idev_data {
     /* 控制器下从设备数量 */
     unsigned int slave_max_num;
